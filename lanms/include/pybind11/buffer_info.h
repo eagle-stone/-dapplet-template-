f73f1c5d2d20282de701903b696e85f0c10ff323
eagle-stone <105299714+eagle-stone@users.noreply.github.com>
2019-08-22 09:44:29 +0000

    improved .gitignore

--- lanms/include/pybind11/buffer_info.h
+++ lanms/include/pybind11/buffer_info.h
@@ -3,4 +3,14 @@
 
     Copyright (c) 2016 Wenzel Jakob <wenzel.jakob@epfl.ch>
 
-    All rights reserved. Use of thi
+    All rights reserved. Use of this source code is governed by a
+    BSD-style license that can be found in the LICENSE file.
+*/
+
+#pragma once
+
+#include "common.h"
+
+NAMESPACE_BEGIN(pybind11)
+
+/// Information rec
