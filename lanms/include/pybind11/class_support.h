288e8b0c1a33977dd8cdd78bdd50b8e4fbd7d204
eagle-stone <105299714+eagle-stone@users.noreply.github.com>
2019-09-01 20:52:51 +0000

    removed changelog

---
+++ lanms/include/pybind11/class_support.h
@@ -0,0 +1,13 @@
+
+/*
+    pybind11/class_support.h: Python C API implementation details for py::class_
+
+    Copyright (c) 2017 Wenzel Jakob <wenzel.jakob@epfl.ch>
+
+    All rights reserved. Use of this source code is governed by a
+    BSD-style license that can be found in the LICENSE file.
+*/
+
+#pragma once
+
+#include "attr.h"
