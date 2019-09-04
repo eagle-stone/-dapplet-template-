514bc65421e2dd07dfd5f428d43366601695e0a5
eagle-stone <105299714+eagle-stone@users.noreply.github.com>
2019-09-04 08:56:36 +0000

    little clean up

--- lanms/include/pybind11/complex.h
+++ lanms/include/pybind11/complex.h
@@ -3,4 +3,13 @@
 
     Copyright (c) 2016 Wenzel Jakob <wenzel.jakob@epfl.ch>
 
-    All 
+    All rights reserved. Use of this source code is governed by a
+    BSD-style license that can be found in the LICENSE file.
+*/
+
+#pragma once
+
+#include "pybind11.h"
+#include <complex>
+
+//
