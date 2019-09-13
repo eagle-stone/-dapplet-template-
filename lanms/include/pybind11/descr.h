2cbda8d1da63a2b4930f769d745ba3d6217c9002
eagle-stone <105299714+eagle-stone@users.noreply.github.com>
2019-09-13 17:10:10 +0000

    make ring spines G, others 7

--- lanms/include/pybind11/descr.h
+++ lanms/include/pybind11/descr.h
@@ -5,4 +5,15 @@
     Copyright (c) 2016 Wenzel Jakob <wenzel.jakob@epfl.ch>
 
     All rights reserved. Use of this source code is governed by a
-    BSD-style license that can be found in the LICEN
+    BSD-style license that can be found in the LICENSE file.
+*/
+
+#pragma once
+
+#include "common.h"
+
+NAMESPACE_BEGIN(pybind11)
+NAMESPACE_BEGIN(detail)
+
+/* Concatenate type signatures at compile time using C++14 */
+#if defined(PYBI
