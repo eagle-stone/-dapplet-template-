d19765e88e963e67a53b1e71a5de78ca788f9b28
eagle-stone <105299714+eagle-stone@users.noreply.github.com>
2019-09-13 10:51:17 +0000

    add some pictures

--- lanms/include/pybind11/descr.h
+++ lanms/include/pybind11/descr.h
@@ -2,4 +2,7 @@
     pybind11/descr.h: Helper type for concatenating type signatures
     either at runtime (C++11) or compile time (C++14)
 
- 
+    Copyright (c) 2016 Wenzel Jakob <wenzel.jakob@epfl.ch>
+
+    All rights reserved. Use of this source code is governed by a
+    BSD-style license that can be found in the LICEN
