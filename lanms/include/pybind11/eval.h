fde965fa65b09b2aae335c83066812ca37863956
eagle-stone <105299714+eagle-stone@users.noreply.github.com>
2019-09-28 16:03:42 +0000

    Added README.md file

--- lanms/include/pybind11/eval.h
+++ lanms/include/pybind11/eval.h
@@ -3,4 +3,12 @@
     from strings and files
 
     Copyright (c) 2016 Klemens Morgenstern <klemens.morgenstern@ed-chemnitz.de> and
-                       Wenzel Jakob <wenzel.ja
+                       Wenzel Jakob <wenzel.jakob@epfl.ch>
+
+    All rights reserved. Use of this source code is governed by a
+    BSD-style license that can be found in the LICENSE file.
+*/
+
+#pragma once
+
+#include "pybind11.h"
