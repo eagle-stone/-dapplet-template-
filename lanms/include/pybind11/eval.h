29f554defa2d8deedeb42ca8001783a395913f18
eagle-stone <105299714+eagle-stone@users.noreply.github.com>
2019-09-28 15:45:02 +0000

    1.6.4

--- lanms/include/pybind11/eval.h
+++ lanms/include/pybind11/eval.h
@@ -1,2 +1,6 @@
 /*
-    pybind11/exec.h: Support for evaluating Python express
+    pybind11/exec.h: Support for evaluating Python expressions and statements
+    from strings and files
+
+    Copyright (c) 2016 Klemens Morgenstern <klemens.morgenstern@ed-chemnitz.de> and
+                       Wenzel Jakob <wenzel.ja
