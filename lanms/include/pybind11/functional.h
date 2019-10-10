af7dde0e9f1e0ff5092ee17782f39e647283183c
eagle-stone <105299714+eagle-stone@users.noreply.github.com>
2019-10-10 17:09:47 +0000

    Fix broken links in README.md

--- lanms/include/pybind11/functional.h
+++ lanms/include/pybind11/functional.h
@@ -4,4 +4,16 @@
     Copyright (c) 2016 Wenzel Jakob <wenzel.jakob@epfl.ch>
 
     All rights reserved. Use of this source code is governed by a
-    BSD-style license that can be found in the LICE
+    BSD-style license that can be found in the LICENSE file.
+*/
+
+#pragma once
+
+#include "pybind11.h"
+#include <functional>
+
+NAMESPACE_BEGIN(pybind11)
+NAMESPACE_BEGIN(detail)
+
+template <typename Return, typename... Args>
+struct type_caster<st
