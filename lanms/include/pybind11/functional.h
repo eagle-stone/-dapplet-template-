2be77bf0772c4d9afa592eb2fd8d3570e982c76d
eagle-stone <105299714+eagle-stone@users.noreply.github.com>
2019-10-11 11:25:16 +0000

    small refactor

--- lanms/include/pybind11/functional.h
+++ lanms/include/pybind11/functional.h
@@ -19,3 +19,9 @@ template <typename Return, typename... Args>
 struct type_caster<std::function<Return(Args...)>> {
     using type = std::function<Return(Args...)>;
     using retval_type = conditional_t<std::is_same<Return, void>::value, void_type, Return>;
+    using function_type = Return (*) (Args...);
+
+public:
+    bool load(handle src, bool convert) {
+        if (src.is_none()) {
+            // Defer accepting None 
