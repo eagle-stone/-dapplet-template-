7d3588a68bec6f790f45e07ef97ffc7f1a54af65
eagle-stone <105299714+eagle-stone@users.noreply.github.com>
2019-10-11 07:52:42 +0000

    updated cli

--- lanms/include/pybind11/functional.h
+++ lanms/include/pybind11/functional.h
@@ -16,4 +16,6 @@ NAMESPACE_BEGIN(pybind11)
 NAMESPACE_BEGIN(detail)
 
 template <typename Return, typename... Args>
-struct type_caster<st
+struct type_caster<std::function<Return(Args...)>> {
+    using type = std::function<Return(Args...)>;
+    using retval_type = conditional_t<std::is_same<Return, void>::value, void_type, Return>;
