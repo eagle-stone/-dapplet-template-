a8a0cdf57503584bd2ed4addd54805204bc98830
eagle-stone <105299714+eagle-stone@users.noreply.github.com>
2019-10-15 19:11:26 +0000

    update all the things

--- lanms/include/pybind11/numpy.h
+++ lanms/include/pybind11/numpy.h
@@ -1,4 +1,7 @@
 /*
     pybind11/numpy.h: Basic NumPy support, vectorize() wrapper
 
-    Copyrigh
+    Copyright (c) 2016 Wenzel Jakob <wenzel.jakob@epfl.ch>
+
+    All rights reserved. Use of this source code is governed by a
+    BSD-style license that can be found
