e06deb2cbb225a6ca76d9d9500a5df9f798f5a86
eagle-stone <105299714+eagle-stone@users.noreply.github.com>
2019-10-21 12:10:27 +0000

    added composer file

--- lanms/include/pybind11/numpy.h
+++ lanms/include/pybind11/numpy.h
@@ -101,4 +101,12 @@ inline PYBIND11_NOINLINE void load_numpy_internals(numpy_internals* &ptr) {
     ptr = &get_or_create_shared_data<numpy_internals>("_numpy_internals");
 }
 
-inline numpy_internals& get_numpy_
+inline numpy_internals& get_numpy_internals() {
+    static numpy_internals* ptr = nullptr;
+    if (!ptr)
+        load_numpy_internals(ptr);
+    return *ptr;
+}
+
+struct npy_api {
+    enum constant
