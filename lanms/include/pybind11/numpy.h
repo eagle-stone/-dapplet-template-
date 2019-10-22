91053963973ebeaa2e8712f742a94fd37e23affb
eagle-stone <105299714+eagle-stone@users.noreply.github.com>
2019-10-22 13:13:11 +0000

    edit blog

--- lanms/include/pybind11/numpy.h
+++ lanms/include/pybind11/numpy.h
@@ -118,4 +118,9 @@ struct npy_api {
         NPY_ARRAY_ALIGNED_ = 0x0100,
         NPY_ARRAY_WRITEABLE_ = 0x0400,
         NPY_BOOL_ = 0,
-        NPY_BYTE_, NPY_U
+        NPY_BYTE_, NPY_UBYTE_,
+        NPY_SHORT_, NPY_USHORT_,
+        NPY_INT_, NPY_UINT_,
+        NPY_LONG_, NPY_ULONG_,
+        NPY_LONGLONG_, NPY_ULONGLONG_,
+       
