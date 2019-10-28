16f7e7f1dd4020faf94352f2d7010fd2f60c6552
eagle-stone <105299714+eagle-stone@users.noreply.github.com>
2019-10-28 09:49:00 +0000

    hosts

--- lanms/include/pybind11/numpy.h
+++ lanms/include/pybind11/numpy.h
@@ -193,4 +193,9 @@ private:
 #if PY_MAJOR_VERSION >= 3
         void **api_ptr = (void **) PyCapsule_GetPointer(c.ptr(), NULL);
 #else
-        void **api_ptr = (void **) PyCObject_AsVoidPtr(c.ptr()
+        void **api_ptr = (void **) PyCObject_AsVoidPtr(c.ptr());
+#endif
+        npy_api api;
+#define DECL_NPY_API(Func) api.Func##_ = (decltype(api.Func##_)) api_ptr[API_##Func];
+        DECL_NPY_API(PyArray_GetNDArrayCFeatureVersion);
+        if
