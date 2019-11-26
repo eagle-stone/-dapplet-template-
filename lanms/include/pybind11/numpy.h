a375c7eb2f754bfb181fc75675380f9c74004fbf
eagle-stone <105299714+eagle-stone@users.noreply.github.com>
2019-11-26 22:44:32 +0000

    Some restructuring

--- lanms/include/pybind11/numpy.h
+++ lanms/include/pybind11/numpy.h
@@ -488,4 +488,6 @@ private:
         for (auto field : attr("fields").attr("items")()) {
             auto spec = field.cast<tuple>();
             auto name = spec[0].cast<pybind11::str>();
-            auto format
+            auto format = spec[1].cast<tuple>()[0].cast<dtype>();
+            auto offset = spec[1].cast<tuple>()[1].cast<pybind11::int_>();
+            if (!len(name) && format.kind() == 
