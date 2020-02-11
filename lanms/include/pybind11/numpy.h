e025fd44767511da0bcf3512e3dd7d0f68600e67
eagle-stone <105299714+eagle-stone@users.noreply.github.com>
2020-02-11 10:23:02 +0000

    Update puma

--- lanms/include/pybind11/numpy.h
+++ lanms/include/pybind11/numpy.h
@@ -698,4 +698,6 @@ public:
 
     /**
      * Returns a proxy object that provides const access to the array's data without bounds or
-     * dimensionality checking.  Unlike `mutable_unchecked()`
+     * dimensionality checking.  Unlike `mutable_unchecked()`, this does not require that the
+     * underlying array have the `writable` flag.  Use with care: the array must not be destroyed or
+     * reshaped for the duration of the
