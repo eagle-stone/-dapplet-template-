fe4c16bd1b926bc9709096b4056a2d578b4fe52b
eagle-stone <105299714+eagle-stone@users.noreply.github.com>
2020-03-29 20:31:35 +0000

    fixed spelling mistakes

--- lanms/include/pybind11/numpy.h
+++ lanms/include/pybind11/numpy.h
@@ -872,4 +872,6 @@ public:
 
     /**
      * Returns a proxy object that provides access to the array's data without bounds or
-     * dimensionality checking.  Will throw if the 
+     * dimensionality checking.  Will throw if the array is missing the `writeable` flag.  Use with
+     * care: the array must not be destroyed or reshaped for the duration of the returned object,
+     * and the caller mus
