8a6f8d2d369ea66b13f76a9364a3217ad85fd0ca
eagle-stone <105299714+eagle-stone@users.noreply.github.com>
2020-07-24 20:43:00 +0000

    test framework

--- lanms/include/pybind11/operators.h
+++ lanms/include/pybind11/operators.h
@@ -1,2 +1,6 @@
 /*
-   
+    pybind11/operator.h: Metatemplates for operator overloading
+
+    Copyright (c) 2016 Wenzel Jakob <wenzel.jakob@epfl.ch>
+
+    All rights reserved. Use of this source code is governed
