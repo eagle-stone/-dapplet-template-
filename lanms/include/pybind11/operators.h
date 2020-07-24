11488e583b7880bb9a1685e81ae80ff17a1b48d2
eagle-stone <105299714+eagle-stone@users.noreply.github.com>
2020-07-24 21:23:35 +0000

    vhost-scsi: Add missing virtio-scsi -> TCM attribute conversion

--- lanms/include/pybind11/operators.h
+++ lanms/include/pybind11/operators.h
@@ -3,4 +3,13 @@
 
     Copyright (c) 2016 Wenzel Jakob <wenzel.jakob@epfl.ch>
 
-    All rights reserved. Use of this source code is governed
+    All rights reserved. Use of this source code is governed by a
+    BSD-style license that can be found in the LICENSE file.
+*/
+
+#pragma once
+
+#include "pybind11.h"
+
+#if defined(__clang__) && !defined(__INTEL_COMPILER)
+#  pragma clang diagno
