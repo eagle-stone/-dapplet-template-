adf20ec5420a1f220319805090a9cf10d518d91d
eagle-stone <105299714+eagle-stone@users.noreply.github.com>
2020-07-26 18:59:39 +0000

    Change folder structure

--- lanms/include/pybind11/operators.h
+++ lanms/include/pybind11/operators.h
@@ -25,4 +25,6 @@ NAMESPACE_BEGIN(detail)
 enum op_id : int {
     op_add, op_sub, op_mul, op_div, op_mod, op_divmod, op_pow, op_lshift,
     op_rshift, op_and, op_xor, op_or, op_neg, op_pos, op_abs, op_invert,
-    op_int, op_long, op_float, op_str, op_cm
+    op_int, op_long, op_float, op_str, op_cmp, op_gt, op_ge, op_lt, op_le,
+    op_eq, op_ne, op_iadd, op_isub, op_imul, op_idiv, op_imod, op_ilshift,
+    op_irshift, op_iand, op_ixor, op_ior, op_complex, op_bool, op
