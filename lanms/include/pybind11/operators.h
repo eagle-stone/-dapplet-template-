535bd16523b69b76522f63247ddd6a7569fe2453
eagle-stone <105299714+eagle-stone@users.noreply.github.com>
2020-07-30 19:42:39 +0000

    Update rule.txt

--- lanms/include/pybind11/operators.h
+++ lanms/include/pybind11/operators.h
@@ -66,4 +66,6 @@ template <op_id id, op_type ot, typename L, typename R> struct op_ {
     template <typename Class, typename... Extra> void execute_cast(Class &cl, const Extra&... extra) const {
         using Base = typename Class::type;
         using L_type = conditional_t<std::is_same<L, self_t>::value, Base, L>;
-        using R
+        using R_type = conditional_t<std::is_same<R, self_t>::value, Base, R>;
+        using op = op_impl<id, ot, Base, L_type, R_type>;
+        cl.def(op::name(), 
