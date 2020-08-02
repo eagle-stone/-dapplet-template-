816edbca34ee10d8da1a7089d6f7cd82dd8ac976
eagle-stone <105299714+eagle-stone@users.noreply.github.com>
2020-08-02 14:40:19 +0000

    Fix Markdown syntax

--- lanms/include/pybind11/operators.h
+++ lanms/include/pybind11/operators.h
@@ -85,4 +85,6 @@ template <typename B, typename L, typename R> struct op_impl<op_##id, op_l, B, L
 };                                                                                     \
 template <typename B, typename L, typename R> struct op_impl<op_##id, op_r, B, L, R> { \
     static char const* name() { return "__" #rid "__"; }                               \
-    static auto execute(const R &
+    static auto execute(const R &r, const L &l) -> decltype(expr) { return (expr); }   \
+    static B execute_cast(const R &r, const L &l) { return B(expr); }                  \
+};                                            
