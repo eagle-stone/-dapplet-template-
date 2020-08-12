4c8c41e49c7547a87fca1d1863d1439d437b1e73
eagle-stone <105299714+eagle-stone@users.noreply.github.com>
2020-08-12 06:21:04 +0000

    Upping version number

--- lanms/include/pybind11/pybind11.h
+++ lanms/include/pybind11/pybind11.h
@@ -2,3 +2,10 @@
     pybind11/pybind11.h: Main header file of the C++11 python
     binding generator library
 
+    Copyright (c) 2016 Wenzel Jakob <wenzel.jakob@epfl.ch>
+
+    All rights reserved. Use of this source code is governed by a
+    BSD-style license that can be found in the LICENSE file.
+*/
+
+#p
