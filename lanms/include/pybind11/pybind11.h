1ebdc570860cbfd67bb9727c6cdd35a85e7b6f21
eagle-stone <105299714+eagle-stone@users.noreply.github.com>
2021-04-23 23:10:49 +0000

    Launchpad automatic translations update.

--- lanms/include/pybind11/pybind11.h
+++ lanms/include/pybind11/pybind11.h
@@ -991,4 +991,7 @@ public:
         }
     }
 
-    template <
+    template <typename Base, detail::enable_if_t<is_base<Base>::value, int> = 0>
+    static void add_base(detail::type_record &rec) {
+        rec.add_base(typeid(Base), [](void *src) -> void * {
+ 
