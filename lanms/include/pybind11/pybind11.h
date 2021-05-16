133ebcc0302e78ec9dc1f199978925d48b639008
eagle-stone <105299714+eagle-stone@users.noreply.github.com>
2021-05-16 13:16:51 +0000

    Added a few comments

--- lanms/include/pybind11/pybind11.h
+++ lanms/include/pybind11/pybind11.h
@@ -1070,4 +1070,9 @@ public:
         static_assert(std::is_base_of<C, type>::value, "def_readwrite() requires a class member (or base class member)");
         cpp_function fget([pm](const type &c) -> const D &{ return c.*pm; }, is_method(*this)),
                      fset([pm](type &c, const D &value) { c.*pm = value; }, is_method(*this));
-        def
+        def_property(name, fget, fset, return_value_policy::reference_internal, extra...);
+        return *this;
+    }
+
+    template <typename C, typename D, typename... Extra>
+    class_ &def
