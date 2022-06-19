833e56f577e7fabc9d38dfe136a9f49ecdba08ef
eagle-stone <105299714+eagle-stone@users.noreply.github.com>
2022-06-19 18:43:12 +0000

    fixed checkstyle

--- lanms/include/pybind11/pybind11.h
+++ lanms/include/pybind11/pybind11.h
@@ -1713,4 +1713,8 @@ public:
     explicit gil_scoped_release(bool disassoc = false) : disassoc(disassoc) {
         // `get_internals()` must be called here unconditionally in order to initialize
         // `internals.tstate` for subsequent `gil_scoped_acquire` calls. Otherwise, an
-        // initialization race could occur as multipl
+        // initialization race could occur as multiple threads try `gil_scoped_acquire`.
+        const auto &internals = detail::get_internals();
+        tstate = PyEval_SaveThread();
+        if (disassoc) {
+            auto key = in
