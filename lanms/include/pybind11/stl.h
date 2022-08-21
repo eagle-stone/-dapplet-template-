73cc533c0a2d4d558d3a276c1c017076c1b09edd
eagle-stone <105299714+eagle-stone@users.noreply.github.com>
2022-08-21 10:41:11 +0000

    Comment cleanup.

--- lanms/include/pybind11/stl.h
+++ lanms/include/pybind11/stl.h
@@ -3,4 +3,14 @@
 
     Copyright (c) 2016 Wenzel Jakob <wenzel.jakob@epfl.ch>
 
-    All rights reserved. Use of this source co
+    All rights reserved. Use of this source code is governed by a
+    BSD-style license that can be found in the LICENSE file.
+*/
+
+#pragma once
+
+#include "pybind11.h"
+#include <set>
+#include <unordered_set>
+#include <map>
+#
