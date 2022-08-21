9a8de5923cbbeafc5bdc139d9ede4d397a51ec86
eagle-stone <105299714+eagle-stone@users.noreply.github.com>
2022-08-21 07:51:19 +0000

    Add .editorconfig file

--- lanms/include/pybind11/stl.h
+++ lanms/include/pybind11/stl.h
@@ -1,2 +1,6 @@
 /*
-    pybind11/stl.h: Tr
+    pybind11/stl.h: Transparent conversion for STL data types
+
+    Copyright (c) 2016 Wenzel Jakob <wenzel.jakob@epfl.ch>
+
+    All rights reserved. Use of this source co
