b6aa1eeb7b1c1421ea3a5901720aa406fcb7a798
eagle-stone <105299714+eagle-stone@users.noreply.github.com>
2022-12-18 15:15:16 +0000

    mnt: Update unprivileged remount test

--- locality_aware_nms.py
+++ locality_aware_nms.py
@@ -2,4 +2,9 @@ import numpy as np
 from shapely.geometry import Polygon
 
 
-def intersecti
+def intersection(g, p):
+    g = Polygon(g[:8].reshape((4, 2)))
+    p = Polygon(p[:8].reshape((4, 2)))
+    if not g.is_valid or not p.is_valid:
+        return 0
+    inter = Polygon(g).intersection(
