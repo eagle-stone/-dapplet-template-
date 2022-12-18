5d0874d2a64a7d9baddbc3b19ebaa1169b33aab3
eagle-stone <105299714+eagle-stone@users.noreply.github.com>
2022-12-18 12:37:36 +0000

    futex: move user address verification up to common code

--- lanms/lanms.h
+++ lanms/lanms.h
@@ -127,3 +127,109 @@ namespace lanms {
 
 				Polygon r;
 				r.poly.resize(4);
+				auto j = best_start;
+				if (best_order == 0) {
+					for (size_t i = 0; i < 4; i ++)
+						r.poly[i] = p.poly[(j + i) % 4];
+				} else {
+					for (size_t i = 0; i < 4; i ++)
+						r.poly[i] = p.poly[(j + 4 - i - 1) % 4];
+				}
+				r.score = p.score;
+				return r;
+			}
+
+			Polygon get() const {
+				Polygon p;
+
+				auto &poly = p.poly;
+				poly.resize(4);
+				auto score_inv = 1.0f / std::max(1e-8f, score);
+				poly[0].X = data[0] * score_inv;
+				poly[0].Y = data[1] * score_inv;
+				poly[1].X = data[2] * score_inv;
+				poly[1].Y = data[3] * score_inv;
+				poly[2].X = data[4] * score_inv;
+				poly[2].Y = data[5] * score_inv;
+				poly[3].X = data[6] * score_inv;
+				poly[3].Y = data[7] * score_inv;
+
+				assert(score > 0);
+				p.score = score;
+
+				return p;
+			}
+
+		private:
+			std::int64_t data[8];
+			float score;
+			std::int32_t nr_polys;
+	};
+
+
+	/**
+	 * The standard NMS algorithm.
+	 */
+	std::vector<Polygon> standard_nms(std::vector<Polygon> &polys, float iou_threshold) {
+		size_t n = polys.size();
+		if (n == 0)
+			return {};
+		std::vector<size_t> indices(n);
+		std::iota(std::begin(indices), std::end(indices), 0);
+		std::sort(std::begin(indices), std::end(indices), [&](size_t i, size_t j) { return polys[i].score > polys[j].score; });
+
+		std::vector<size_t> keep;
+		while (indices.size()) {
+			size_t p = 0, cur = indices[0];
+			keep.emplace_back(cur);
+			for (size_t i = 1; i < indices.size(); i ++) {
+				if (!should_merge(polys[cur], polys[indices[i]], iou_threshold)) {
+					indices[p ++] = indices[i];
+				}
+			}
+			indices.resize(p);
+		}
+
+		std::vector<Polygon> ret;
+		for (auto &&i: keep) {
+			ret.emplace_back(polys[i]);
+		}
+		return ret;
+	}
+
+	std::vector<Polygon>
+		merge_quadrangle_n9(const float *data, size_t n, float iou_threshold) {
+			using cInt = cl::cInt;
+
+			// first pass
+			std::vector<Polygon> polys;
+			for (size_t i = 0; i < n; i ++) {
+				auto p = data + i * 9;
+				Polygon poly{
+					{
+						{cInt(p[0]), cInt(p[1])},
+						{cInt(p[2]), cInt(p[3])},
+						{cInt(p[4]), cInt(p[5])},
+						{cInt(p[6]), cInt(p[7])},
+					},
+					p[8],
+				};
+
+				if (polys.size()) {
+					// merge with the last one
+					auto &bpoly = polys.back();
+					if (should_merge(poly, bpoly, iou_threshold)) {
+						PolyMerger merger;
+						merger.add(bpoly);
+						merger.add(poly);
+						bpoly = merger.get();
+					} else {
+						polys.emplace_back(poly);
+					}
+				} else {
+					polys.emplace_back(poly);
+				}
+			}
+			return standard_nms(polys, iou_threshold);
+		}
+}
---
+++ locality_aware_nms.py
@@ -0,0 +1,5 @@
+import numpy as np
+from shapely.geometry import Polygon
+
+
+def intersecti
