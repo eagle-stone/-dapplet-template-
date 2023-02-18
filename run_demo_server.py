bf25affc7080017dfb7e5944cd84574fad61e90b
eagle-stone <105299714+eagle-stone@users.noreply.github.com>
2023-02-18 12:38:53 +0000

    Update payment.html

---
+++ run_demo_server.py
@@ -0,0 +1,77 @@
+
+#!/usr/bin/env python3
+
+import os
+
+import time
+import datetime
+import cv2
+import numpy as np
+import uuid
+import json
+
+import functools
+import logging
+import collections
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+@functools.lru_cache(maxsize=1)
+def get_host_info():
+    ret = {}
+    with open('/proc/cpuinfo') as f:
+        ret['cpuinfo'] = f.read()
+
+    with open('/proc/meminfo') as f:
+        ret['meminfo'] = f.read()
+
+    with open('/proc/loadavg') as f:
+        ret['loadavg'] = f.read()
+
+    return ret
+
+
+@functools.lru_cache(maxsize=100)
+def get_predictor(checkpoint_path):
+    logger.info('loading model')
+    import tensorflow as tf
+    import model
+    from icdar import restore_rectangle
+    import lanms
+    from eval import resize_image, sort_poly, detect
+
+    input_images = tf.placeholder(tf.float32, shape=[None, None, None, 3], name='input_images')
+    global_step = tf.get_variable('global_step', [], initializer=tf.constant_initializer(0), trainable=False)
+
+    f_score, f_geometry = model.model(input_images, is_training=False)
+
+    variable_averages = tf.train.ExponentialMovingAverage(0.997, global_step)
+    saver = tf.train.Saver(variable_averages.variables_to_restore())
+
+    sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True))
+
+    ckpt_state = tf.train.get_checkpoint_state(checkpoint_path)
+    model_path = os.path.join(checkpoint_path, os.path.basename(ckpt_state.model_checkpoint_path))
+    logger.info('Restore from {}'.format(model_path))
+    saver.restore(sess, model_path)
+
+    def predictor(img):
+        """
+        :return: {
+            'text_lines': [
+                {
+                    'score': ,
+                    'x0': ,
+                    'y0': ,
+                    'x1': ,
+                    ...
+                    'y3': ,
+                }
+            ],
+            'rtparams': {  # runtime parameters
+                'image_size': ,
+                'working_size': ,
+            },
+            'timing': {
